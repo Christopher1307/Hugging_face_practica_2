ef0dd068061dc741be70cc3f05810a24912860b3
gabrieltenerife <gabrieltenerife123@gmail.com>
2025-10-23 18:27:35 +0100

    Proyecto

--- back/Backend.py
+++ back/Backend.py
@@ -2,18 +2,29 @@ import gradio as gr
 import torch
 from transformers import pipeline
 
+#Audio-Texto
 asr = pipeline(task="automatic-speech-recognition", model="openai/whisper-small",device="cuda" if torch.cuda.is_available() else "cpu")
 audio_a_texto = None
 
+# Traductor
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
+
+
 def transcribe(audio):
 
+    # Audio-Texto
     global audio_a_texto
     if audio is None:
         return "Por favor, sube un archivo de audio."
 
     audio_a_texto = asr(audio)["text"]
-    print(audio_a_texto)
-    return audio_a_texto
+
+    # Traductor
+
+    resultado = pipe(audio_a_texto)
+
+
+    return resultado
 
 gr.Interface(
     fn=transcribe,
